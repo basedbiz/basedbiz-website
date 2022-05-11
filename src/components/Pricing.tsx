748b8ad8264e4feb30e21e22f095b749f90c5aa1
Nathaniel Gladstone <nathanielkgl@gmail.com>
2022-05-10 21:35:06 -0700

    test

---
+++ src/components/Pricing.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import config from '../config/index.json';
+
+const Pricing = () => {
+  const { pricing } = config;
+  const { items, title } = pricing;
+  const [firstPlan] = items;
+
+  return (
+    <section className={`bg-background py-8`} id="pricing">
+      <div className={`container mx-auto px-2 pt-4 pb-12 text-primary`}>
+        <h1
+          className={`w-full my-2 text-5xl font-bold leading-tight text-center text-primary`}
+        >
+          {title}
+        </h1>
+        <div className={`w-full mb-4`}>
+          <div
+            className={`h-1 mx-auto bg-primary w-64 opacity-25 my-0 py-0 rounded-t`}
+          ></div>
+        </div>
+        <div
+          className={`flex flex-col sm:flex-row justify-center pt-12 my-12 sm:my-4`}
+        >
+          <div
+            className={`flex flex-col w-5/6 lg-1/4 mx-auto lg:mx-0 rounded-none lg:rounded-l-lg bg-background mt-4`}
+          >
+            <div
+              className={`flex-1 bg-background text-gray-600 rounded-t rounded-b-none overflow-hidden shadow`}
+            >
+              <div className={`p-8 text-3xl font-bold text-center border-b-4`}>
+                {firstPlan?.name}
+              </div>
+              <ul className={`w-full text-center text-sm`}>
+                {firstPlan?.features.map((feature) => (
+                  <li
+                    className={`border-b py-4`}
+                    key={`${firstPlan.name}-${feature}`}
+                  >
+                    {feature}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+            <div className={`flex-1 bg-background rounded-t rounded-b-none overflow-hidden shadow`}>
+            </div>
+            <div className={`flex-none mt-auto bg-background rounded-b rounded-t-none overflow-hidden shadow p-6`}>
+              </div>
+            </div>
+          <div className={`flex flex-col w-5/6 lg:w-1/4 mx-auto lg:mx-0 rounded-none lg:rounded-l-lg bg-primary mt-4`}>
+          </div>
+        </div>
+    </section>
+  );
+};
+
+export default Pricing;
